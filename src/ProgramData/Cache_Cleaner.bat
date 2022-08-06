7df15efe25a0668dee6775ae87a46ae21149fd11
Fiki <89738697+fikinoob@users.noreply.github.com>
2022-08-06 11:40:05 +0000

    Add files via upload

---
+++ src/ProgramData/Cache_Cleaner.bat
@@ -0,0 +1,15 @@
+set temp=C:\Users\Duck\AppData\Local\Temp
+del /F /Q C:\Windows\SystemApps\Microsoft.Windows.StartMenuExperienceHost_cw5n1h2txyewy
+del /F /Q C:\Windows\SystemApps\Microsoft.Windows.Search_cw5n1h2txyewy
+del /s /f /q c:\windows\temp\*.*
+del /s /f /q C:\WINDOWS\Prefetch\*.*
+del /s /f /q %temp%\*.*
+del /f /q c:\windows\tempor~1
+del /f /q c:\windows\temp
+del /f /q c:\windows\tmp
+del /f /q c:\windows\ff*.tmp
+del /f /q c:\windows\history
+del /f /q c:\windows\cookies
+del /f /q c:\windows\recent
+del /f /q c:\windows\spool\printers
+del *.log /a /s /q /f
