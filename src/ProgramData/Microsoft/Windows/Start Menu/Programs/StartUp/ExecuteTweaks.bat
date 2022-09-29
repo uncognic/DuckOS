8b7109baae584ca02388d1b7ee9a01b4b6a97e5f
Fiki <fikinoob9@gmail.com>
2022-09-29 19:49:39 +0200

    remove autologon... oobe coming soon

--- src/ProgramData/Microsoft/Windows/Start Menu/Programs/StartUp/ExecuteTweaks.bat
+++ src/ProgramData/Microsoft/Windows/Start Menu/Programs/StartUp/ExecuteTweaks.bat
@@ -1,9 +1,3 @@
-@echo off
-
-:: Set up autologon.
-echo Y|reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v "AutoAdminLogon" /t REG_DWORD /d "1" /f
-echo Y|reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v "DefaultUserName" /t REG_SZ /d Duck /f
-echo Y|reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v "DefaultPassword" /t REG_SZ /d "" /f
 
 :: Start the post script
 cd /d C:\Windows\DuckOS_Modules
