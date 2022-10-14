edb27202e729ccabd3ccbf074136708005aee545
AnhNguyenlost13 <94160753+AnhNguyenlost13@users.noreply.github.com>
2022-10-14 22:49:46 +0700

    Only use -isDuck
    in script launcher

--- src/ProgramData/Microsoft/Windows/Start Menu/Programs/StartUp/ExecuteTweaks.bat
+++ src/ProgramData/Microsoft/Windows/Start Menu/Programs/StartUp/ExecuteTweaks.bat
@@ -1,5 +1,5 @@
 :: Start the post script
-start "" "%SystemRoot%\DuckOS_Modules\nsudo.exe" -U:T -P:E "C:\Windows\DuckOS_Modules\DuckOS-post_script.bat" -isDuck -tweakOnly
+start "" "%SystemRoot%\DuckOS_Modules\nsudo.exe" -U:T -P:E "C:\Windows\DuckOS_Modules\DuckOS-post_script.bat" -isDuck
 
 :: Delete this batch file & exit
 del /F /Q %0
